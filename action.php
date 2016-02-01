9eef583ab081809ca9085fee88a209876ac93c1e
alexaandrov <alexaandrov@gmail.com>
2016-02-02 00:57:43 +0300

    fixes

--- action.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if ($_POST['']); 
-$file=fopen('mes.txt','a+'); 
-fputs($file,$_POST['name']); 
-fputs($file,$_POST['email']); 
-fputs($file,$_POST['mes']); 
-fclose($file); 
-header("Location:index.php");
-?>
